0e32182671f1fe37073d4f5d70cdb1a197c73f3b
DafyBotz12 <92973411+DafyBotz12@users.noreply.github.com>
2021-10-30 18:48:46 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,42 @@
+let fs = require('fs')
+let fetch = require('node-fetch')
+global.tum = fs.readFileSync('./src/Ah5.jpeg')
+global.tum2 = fs.readFileSync('./src/emror.jpeg')
+global.thumb = 'https://i.ibb.co/34PV5yH/By-Relldev.jpg'
+global.thumb2 = fs.readFileSync('./src/emror.jpeg')
+global.owner = ['6281379753850', '6283820073017', '6283801499848','62813828362494','85290882526','6285240750713','6282125986924','6288286421519'] // Put your number here
+global.mods = ['6281379753850'] // Want some help?
+global.prems = ['6281379753850','6285893120588','6285656615441'] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com', 
+  Dehan: 'https://dhnjing.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': '879aaf8cc437fcb4',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+  'https://dhnjing.xyz': 'LitRHap'
+}
+
+// Sticker WM
+global.packname = 'I hope you\'re fine'
+global.author = 'LitRHap'
+global.multiplier = 69 // The higher, The harder levelup
+
+let chalk = require('chalk')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
